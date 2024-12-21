d90f186398b6f7e01543c49847b8afdef98de202
Elia-Haider <haiderhashmi11220@gmail.com>
2024-12-21 22:38:16 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const mods = [
     { id: 1, title: "Free Inventory Management Mod for Minecraft Pe 1.19/1.21", image: "https://i.postimg.cc/T1Yrhz6k/a-thumbnail-style-image-with-a-blurred-m-Qyh-Ksla-JQ6-Wv7v-GXCd-VGRQ-Hw5l6t8x-S-q-FQ5-Lq-GX-Brg.jpg", type: "mods", link: "mods/mod1.html" },
-    { id: 2, title: "One-Click House Mod for Minecraft pe 1.10/21", image: "", type: "mods", link: "mods/mod2.html" },
+    { id: 2, title: "One-Click House Mod for Minecraft pe 1.10/21", image: "https://i.postimg.cc/kM1Sfh9m/1734802246435-85720973153157280467876478870037721941.png", type: "mods", link: "mods/mod2.html" },
     { id: 3, title: "Resource Pack 1", image: "images/resource1.jpg", type: "resources", link: "resources/resource1.html" },
     { id: 4, title: "Map 1", image: "images/map1.jpg", type: "maps", link: "maps/map1.html" },
     { id: 5, title: "Map 2", image: "images/map2.jpg", type: "maps", link: "maps/map2.html" },
